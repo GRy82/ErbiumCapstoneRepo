fc3e2101f321f3c8a0fb4a08e55299914ab45027
zbiedinger <zbiedinger@mac.com>
2021-01-15 14:25:33 -0500

    Added Models

--- ErbiumCapstone/Models/Contractor.cs
+++ ErbiumCapstone/Models/Contractor.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Identity;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +10,20 @@ namespace ErbiumCapstone.Models
 {
     public class Contractor
     {
+        [Key]
+        public int ContractorId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string StreetAddress { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public int ZipCode { get; set; }
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+
+
+        [ForeignKey("IdentityUser")]
+        public string IdentityUserId { get; set; }
+        public IdentityUser IdentityUser { get; set; }
     }
 }
---
+++ ErbiumCapstone/Models/ContractorSkills.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ErbiumCapstone.Models
+{
+    public class ContractorSkill
+    {
+        [Key]
+        public int ContractorSkillId { get; set; }
+
+        [ForeignKey("Skill")]
+        public int SkillId { get; set; }
+        public Skill Skill { get; set; }
+
+
+        [ForeignKey("Contractor")]
+        public int ContractorId { get; set; }
+        public Contractor Contractor { get; set; }
+    }
+}
