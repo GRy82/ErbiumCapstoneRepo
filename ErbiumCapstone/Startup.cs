66254b51853e6dd9c580fe72cf0d0e453b11404b
JoeYashinsky <joey.yashinsky@gmail.com>
2021-01-15 13:53:03 -0500

    Updated services in ConfigureServices method inside the Startup.cs file to reflect the prior changes made to RegisterModel class

--- ErbiumCapstone/Startup.cs
+++ ErbiumCapstone/Startup.cs
@@ -32,8 +32,10 @@ namespace ErbiumCapstone
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(
                     Configuration.GetConnectionString("DefaultConnection")));
-            services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
-                .AddEntityFrameworkStores<ApplicationDbContext>();
+            services.AddIdentity<IdentityUser, IdentityRole>(options => options.SignIn.RequireConfirmedAccount = false)
+                .AddEntityFrameworkStores<ApplicationDbContext>()
+                .AddDefaultUI()
+                .AddDefaultTokenProviders();
             services.AddControllersWithViews();
             services.AddRazorPages();
             services.AddScoped<IRepositoryWrapper, RepositoryWrapper>();
