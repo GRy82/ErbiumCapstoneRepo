837def2d6be1b12df67aae27d5f7243ab14d268a
phher802 <your_email@example.com>
2021-01-17 13:48:33 -0600

    Add SignalR client side library and create folder and class for ChatHub

---
+++ ErbiumCapstone/SignalR/Hubs/ChatHub.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+
+namespace ErbiumCapstone.SignalR.Hubs
+{
+    public class ChatHub : Hub //hub class manages connections, groups, and messaging
+    { 
+        public async Task SendMessage(string user, string message)
+        {   //can be called by a connected client to send a message to all clients
+            await Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
+    }
+}
--- ErbiumCapstone/Startup.cs
+++ ErbiumCapstone/Startup.cs
@@ -49,6 +49,7 @@ namespace ErbiumCapstone
             services.AddRazorPages();
 
             services.AddScoped<IRepositoryWrapper, RepositoryWrapper>();
+            services.AddSignalR();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
