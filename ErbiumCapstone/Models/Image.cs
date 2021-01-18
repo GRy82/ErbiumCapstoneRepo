9769e0fb132b2354e3e68595eb7aeee39771763c
JoeYashinsky <joey.yashinsky@gmail.com>
2021-01-18 11:35:27 -0500

    added a model (Image) to serve as a junction table

---
+++ ErbiumCapstone/Models/Image.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ErbiumCapstone.Models
+{
+    public class Image
+    {
+        [Key]
+        public int ImageId { get; set; }
+
+        public string ImageTitle { get; set; }
+
+        public IFormFile ImageFile { get; set; }
+    }
+}
