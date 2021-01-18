7ff05a6401bd5066dd32ae4c3109200cc41f0b78
JoeYashinsky <joey.yashinsky@gmail.com>
2021-01-18 15:40:53 -0500

    continue work with look, sidebar of different pages

---
+++ ErbiumCapstone/ApiKeys 2.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ErbiumCapstone
+{
+    public static class ApiKeys
+    {
+
+        private static string GeocodingKey = "AIzaSyDa6eBri2JBpz16e2RMCyNUxWN5Ay_FPis";
+        private static string MessagingKey = "";
+
+        public static string GetGeocodingKey()
+        {
+            return GeocodingKey;
+        }
+
+        public static string GetMessagingKey()
+        {
+            return MessagingKey;
+        }
+    }
+}
