2920e66b2a09056cf0b16acd93ada407f7cd6142
GRy82 <gwryan@buffalo.edu>
2021-01-18 12:57:08 -0500

    Display attibutes added to Job model. ProvideUpdates, ProvidePix and NumberOfUpdates inputs added into CreateJob view. The last of which generates dynamically with a true ProvideUpdates.

--- ErbiumCapstone/Models/Job.cs
+++ ErbiumCapstone/Models/Job.cs
@@ -12,8 +12,10 @@ namespace ErbiumCapstone.Models
         [Key]
         public int JobId { get; set; }
 
+        [Display(Name = "Job Name")]
         public string JobName { get; set; }
 
+        [Display(Name = "Job Type")]
         public string JobType { get; set; }
 
 
@@ -26,16 +28,28 @@ namespace ErbiumCapstone.Models
         public Contractor Contractor { get; set; }
 
         public string Description { get; set; }
+
+        [Display(Name = "Cost")]
         public double ProposedCost { get; set; }
         public double AgreedCost { get; set; }
         public DateTime? DeadLine { get; set; }
+
+        [Display(Name = "Start Date")]
         public DateTime? JobStart { get; set; }
+
+        [Display(Name = "Completion Date")]
         public DateTime? JobCompletion { get; set; }
         public TimeSpan? JobDuration { get; set; }
         public bool isJobCompletionApproved { get; set; }
         public string JobState { get; set; }
+
+        [Display(Name = "Provide Updates")]
         public bool ProvideUpdates { get; set; }
+
+        [Display(Name = "Provide Progress Pictures")]
         public bool ProvidePix { get; set; }
+
+        [Display(Name = "Updates Required")]
         public int NumberOfUpdates { get; set; }
         public bool CustomerAcceptedJob { get; set; }
         public bool ContractorAcceptedJob { get; set; }
